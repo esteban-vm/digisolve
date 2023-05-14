4e76126a9032a3a30bd31547619a98296e85cfd5
esteban-vm <estebanvm90@outlook.com>
2023-05-14 16:34:49 -0400

    fixing asset issue

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const assetPrefix = `/${repo}/`
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath,
-  assetPrefix,
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 }
 
 module.exports = nextConfig
