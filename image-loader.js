989908b96c36989093d1bf72cc6849f2c113d4ae
esteban-vm <estebanvm90@outlook.com>
2023-05-15 16:09:35 -0400

    refactors

--- image-loader.js
+++
@@ -1,1 +0,0 @@
-export default ({ src }) => `https://res.cloudinary.com/devm5dl9j/image/upload${src}`
--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let path = ''
-let prefix = '/'
-let imageSource = undefined
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  path = `/${repo}`
-  prefix = `/${repo}/`
-  imageSource = { loader: 'custom', loaderFile: './image-loader.js' }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: path,
-  assetPrefix: prefix,
-  images: imageSource,
+  basePath: '',
+  assetPrefix: '/',
 }
 
 module.exports = nextConfig
